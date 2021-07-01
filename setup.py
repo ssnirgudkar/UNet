3fae6f66d2a18ce92e6e9f73e746be8fad1daf9a
Shailesh Nirgudkar <shailesh.nirgudkar@gmail.com>
2021-07-01 08:48:00 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='unetpackage',
+   version='1.0',
+   description='Unet code package',
+   author='shailesh',
+   author_email='foomail@foo.com',
+   packages=['unetpackage'],  #same as name
+   install_requires=[ ], #external packages as dependencies
+)
